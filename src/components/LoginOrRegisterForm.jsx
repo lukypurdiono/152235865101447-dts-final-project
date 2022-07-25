76c1d1af22105658d65cc47a229342992fccedbe
lukypurdiono <lukypurdiono@gmail.com>
2022-07-25 22:13:30 +0700

    fixing interface login and register

--- src/components/LoginOrRegisterForm.jsx
+++ src/components/LoginOrRegisterForm.jsx
@@ -10,20 +10,11 @@ import {
 
 import { useAuthState } from "react-firebase-hooks/auth";
 
+// import LogosGoogleIcon from "../assets/LogosGoogleIcon";
+
 const LoginOrRegisterForm = ({ loginOrRegister }) => {
   const navigate = useNavigate();
 
-  // di sini kita akan menggunakan hooks useAuthState
-  // useAuthState ini menerima 2 parameter:
-  // parameter 1: auth (yang kita buat dan export dari firebase)
-  // parameter 2 (optional): options (dalam bentuk object)
-  //    digunakan apabila ingin menggunakan hooks dengan lebih detail (melihat perubahan user)
-  //    (Pada pembelajaran ini tidak digunakan)
-
-  // Mengembalikan 3 data (dalam array)
-  // user: akan mengembalikan auth.User apabila ada yang log in, dan null bila tidak ada
-  // loading: boolean yang digunakan sebagai indikator apakah firebasenya sedang menunggu login
-  // error: bila ada error yang diberikan
   const [user, isLoading] = useAuthState(auth);
 
   const [credential, setCredential] = useState({
@@ -46,22 +37,10 @@ const LoginOrRegisterForm = ({ loginOrRegister }) => {
   };
 
   const loginHandler = () => {
-    // console.log("Login");
-    // navigate("/");
-
-    // Kita di sini tidak menggunakan navigate ke login lagi,
-    // karena pada firebase, ketika selesai login,
-    // maka auth statenya akan otomatis berubah (hooks useAuthState, data user)
     loginDenganEmailDanPassword(credential.email, credential.password);
   };
 
   const registerHandler = () => {
-    // console.log("Register");
-    // navigate("/login");
-
-    // Kita di sini tidak menggunakan navigate ke login lagi, karena pada Firebase
-    // Ketika selesai register akan otomatis login juga
-    // dan auth statenya akan otomatis berubah (hooks useAuthState, data user)
     registerDenganEmailDanPassword(credential.email, credential.password);
   };
 
@@ -73,65 +52,94 @@ const LoginOrRegisterForm = ({ loginOrRegister }) => {
     }
   };
 
-  // Lalu sekarang bagaimana kita track orang yang sedang login, dan apabila ada yang login
-  // kita pindahkan ke halaman utama?
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
 
-  // Kita gunakan.... useEffect !
-  useEffect(
-    () => {
-      // Nah di sini mungkin kita bisa modifikasi / menggunakan
-      // isLoading sebagai logic untuk menampilkan loading screen
-      // (pada pembelajaran ini loading screen tidak dibuat)
-      if (isLoading) {
-        // Tampilkan loading screen (bila ada)
-        return;
-      }
-
-      // Lalu apabila usernya ditemukan (ada / tidak null)
-      // Maka akan kita navigasikan ke halaman HomePage
-      if (user) {
-        navigate("/");
-      }
-    },
-    // Sekarang dependency kita tergantung pada user dan isLoading dari useAuthState
-    [user, isLoading, navigate]
-  );
+    if (user) {
+      navigate("/");
+    }
+  }, [user, isLoading, navigate]);
 
   return (
-    <div className="flex h-full bg-bcolor">
-      <div className="flex bg-primary h-4/6 w-4/5 m-auto">
-        <div className="flex w-1/2 bg-secondary">
-          <img src={require("../assets/book.jpg")} alt="book recipe" />
+    <div className="flex h-full bg-bcolor overflow-hidden">
+      <div className="flex h-5/6 w-4/5 m-auto ">
+        <div className="flex w-1/2 bg-primary rounded-l-xl">
+          {/* <img src={require("../assets/book.jpg")} alt="book recipe" /> */}
         </div>
-        <div className="flex flex-col w-1/2 justify-center p-10">
-          <h1>
-            {loginOrRegister === "login" ? "Login Page" : "Register Page"}
-          </h1>
-          <h2>Email</h2>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={credential.email}
-            onChange={textFieldEmailOnChangeHandler}
-            className="border"
-          />
-          <h2>Password</h2>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={credential.password}
-            onChange={textFieldPasswordOnChangeHandler}
-            className="border"
-          />
-          <input type="button" value="" />
-          <button
-            onClick={buttonLoginOrRegisterOnClickHandler}
-            className="bg-primary border border-radius-xl"
-          >
-            {loginOrRegister === "login" ? "Login" : "Register Account"}
-          </button>
+        <div className="flex flex-col w-1/2 bg-tertiary rounded-r-xl items-center">
+          <div className="flex flex-col justify-center h-full w-3/4 ">
+            {/* title start*/}
+            <div className="flex flex-col h-2/5 justify-center">
+              <h1 className="text-4xl font-bold text-primary mb-4 ">
+                {loginOrRegister === "login" ? "Login Page" : "Register Page"}
+              </h1>
+
+              <p className="text-slate-500">
+                Already have an account?
+                <span className="text-primary font-bold"> Log In</span>
+              </p>
+
+              <div className="flex justify-between mt-5">
+                <button className="bg-white p-2 rounded-md w-1/2 border border-slate-300">
+                  {/* <LogosGoogleIcon /> */}
+                  Sign Up With Google
+                </button>
+                <button className="bg-blue-600 text-slate-100 rounded-md p-2 w-1/2 ml-2 border-none">
+                  Sign Up With Facebook
+                </button>
+              </div>
+              <hr className="mt-10 border" />
+            </div>
+            {/* title end */}
+
+            {/* eform start */}
+            <div className="flex flex-col h-3/5">
+              <h2 className="text-lg mb-2 text-slate-700">Email address</h2>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                value={credential.email}
+                onChange={textFieldEmailOnChangeHandler}
+                className="border p-2 rounded-md focus:outline-none"
+              />
+              <h2 className="text-lg mb-2 text-slate-700 mt-2">Password</h2>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={credential.password}
+                onChange={textFieldPasswordOnChangeHandler}
+                className="border p-2 rounded-md focus:outline-none"
+              />
+              {/* <input type="checkbox" name="" id="" /> <p></p> */}
+              <div class="flex items-center mt-2 mb-2">
+                <input
+                  id="link-checkbox"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                />
+                <label
+                  for="link-checkbox"
+                  className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                >
+                  I agree with
+                  <span className="text-primary"> Terms of Services</span> and
+                  <span className="text-primary"> Privacy Policy</span>
+                </label>
+              </div>
+              <button
+                onClick={buttonLoginOrRegisterOnClickHandler}
+                className="bg-primary border rounded-xl p-3 text-slate-100 mt-4"
+              >
+                {loginOrRegister === "login" ? "Login" : "Register Account"}
+              </button>
+            </div>
+            {/* form end */}
+          </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    colors: {
-      primary: "#F38181",
-      secondary: "#FCE38A",
-      tertiary: "#EAFFD0",
-      bcolor: "#95E1D3",
+    extend: {
+      colors: {
+        primary: "#F38181",
+        secondary: "#FCE38A",
+        tertiary: "#EAFFD0",
+        bcolor: "#95E1D3",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
     },
-    fontFamily: {
-      poppins: ["Poppins", "sans-serif"],
-    },
-    extend: {},
   },
   plugins: [],
 };
